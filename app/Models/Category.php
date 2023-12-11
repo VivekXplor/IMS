6ac621f94466e002ad16e1b2b2a9e1b22dd52ac3
Vikzdimension <48516590+Vikzdimension@users.noreply.github.com>
2023-12-11 15:27:49 +0530

    Model, Migration, and Routes created for items and categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];        
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Item extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+    ];    
 }
